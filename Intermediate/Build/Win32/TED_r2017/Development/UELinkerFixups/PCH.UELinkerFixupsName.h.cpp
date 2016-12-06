4e6a31a00cde7e56d155a70a49d842e600a022fe
Davi <daviuai25@gmail.com>
2016-12-06 02:08:29 -0200

    Patch 0.3.0 BASE

--- Intermediate/Build/Win32/TED_r2017/Development/UELinkerFixups/PCH.UELinkerFixupsName.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:\Users\daviu\Documents\Ted_r2017_1\Intermediate\Build\Win32\UE4\Development\UELinkerFixupsName.h"
--- Intermediate/Build/Win32/TED_r2017/Development/UELinkerFixups/UELinkerFixupsName.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_UELINKERFIXUPSNAME_H__
-#define __AUTO_UELINKERFIXUPSNAME_H__
-//Last Write: 12/6/2016 3:48:47 AM +00:00
-#include "C:\Users\daviu\Documents\Ted_r2017_1\Intermediate\Build\Win32\UE4\Development\UELinkerFixupsName.h"
-#endif//__AUTO_UELINKERFIXUPSNAME_H__
